701a1093d70e5dbc33cc863b67a0b00bbab2e935
ankoi0310 <huynhvahuuan3620@gmail.com>
2023-02-25 17:23:02 +0700

    Register screen

---
+++ lib/component/custom_button.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:movie_ticket_booking_flutter_nlu/config/size_config.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+    super.key,
+    required this.title,
+    required this.width,
+    required this.height,
+    required this.onPressed,
+  });
+
+  final String title;
+  final double width;
+  final double height;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Expanded(
+          child: ElevatedButton(
+            onPressed: onPressed,
+            style: ElevatedButton.styleFrom(
+              fixedSize: Size(width, height),
+              backgroundColor: Theme.of(context).primaryColor,
+            ),
+            child: Text(
+              'Create Account',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: getProportionateScreenWidth(18),
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
