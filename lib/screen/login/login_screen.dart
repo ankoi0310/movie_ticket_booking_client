feca5553eef6faba6231b822f05e07d881dc502e
ankoi0310 <huynhvahuuan3620@gmail.com>
2023-02-28 20:29:27 +0700

    login screen

---
+++ lib/screen/login/login_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:movie_ticket_booking_flutter_nlu/config/size_config.dart';
+import 'package:movie_ticket_booking_flutter_nlu/screen/change_password/component/Body.dart';
+
+class LoginScreen extends StatefulWidget {
+  static const routeName = '/login';
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return const SafeArea(
+      child: Scaffold(
+        body: Body(),
+      ),
+    );
+  }
+}
