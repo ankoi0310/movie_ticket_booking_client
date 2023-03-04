a13ad0b740d6c170765b6271c1766709d9760e91
catabada <huynhhuuan10121972@gmail.com>
2023-03-04 11:38:24 +0700

    rework home screen

--- lib/config/routes.dart
+++ lib/config/routes.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:movie_ticket_booking_flutter_nlu/screen/change_password/change_password_screen.dart';
-import 'package:movie_ticket_booking_flutter_nlu/screen/login/login_screen.dart';
-import 'package:movie_ticket_booking_flutter_nlu/screen/profile/profile_screen.dart';
-import 'package:movie_ticket_booking_flutter_nlu/screen/register/register_screen.dart';
+import 'package:movie_ticket_booking_flutter_nlu/layout/default_layout.dart';
+import 'package:movie_ticket_booking_flutter_nlu/screen/home/home_screen.dart';
+import 'package:movie_ticket_booking_flutter_nlu/screen/movie_detail/movie_detail_screen.dart';
 
 final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
   // SplashScreen.routeName: (BuildContext context) => const SplashScreen(),
-  RegisterScreen.routeName: (BuildContext context) => const RegisterScreen(),
-  LoginScreen.routeName: (BuildContext context) => const LoginScreen(),
-  ProfileScreen.routeName: (BuildContext context) => const ProfileScreen(),
-  ChangePasswordScreen.routeName: (BuildContext context) => const ChangePasswordScreen(),
+  // RegisterScreen.routeName: (BuildContext context) => const RegisterScreen(),
+  // ProfileScreen.routeName: (BuildContext context) => const ProfileScreen(),
+  HomeScreen.routeName: (BuildContext context) =>
+      const DefaultLayout(child: HomeScreen()),
+  MovieDetailScreen.routeName: (BuildContext context) => MovieDetailScreen(),
 };
--- lib/config/size_config.dart
+++ lib/config/size_config.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:movie_ticket_booking_flutter_nlu/config/responsive.dart';
 
 class SizeConfig {
+  static late double width;
+  static late double height;
   static late MediaQueryData _mediaQueryData;
   static late double screenWidth;
   static late double screenHeight;
@@ -12,20 +15,32 @@ class SizeConfig {
     screenWidth = _mediaQueryData.size.width;
     screenHeight = _mediaQueryData.size.height;
     orientation = _mediaQueryData.orientation;
+
+    if (Responsive.isMobile(context)) {
+      width = 720;
+      height = 1280;
+    } else if (Responsive.isTablet(context)) {
+      width = 1280;
+      height = 800;
+    } else {
+      width = 1920;
+      height = 1080;
+    }
   }
 }
 
 // Get the proportionate height as per screen size
 double getProportionateScreenHeight(double inputHeight) {
   double screenHeight = SizeConfig.screenHeight;
-  // 896 is the layout height that designer use
-  // or you can say iPhone 11
-  return (inputHeight / 896.0) * screenHeight;
+  double height = SizeConfig.height;
+
+  return (inputHeight / height) * screenHeight;
 }
 
 // Get the proportionate height as per screen size
 double getProportionateScreenWidth(double inputWidth) {
   double screenWidth = SizeConfig.screenWidth;
-  // 414 is the layout width that designer use
-  return (inputWidth / 414.0) * screenWidth;
+  double width = SizeConfig.width;
+
+  return (inputWidth / width) * screenWidth;
 }
