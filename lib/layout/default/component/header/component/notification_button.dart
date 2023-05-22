25c41a3424a838c574d54a0c051f1d564781f68f
catabada <huynhhuuan10121972@gmail.com>
2023-05-22 14:30:03 +0700

    notice service, checkout have user

--- lib/layout/default/component/header/component/notification_button.dart
+++ lib/layout/default/component/header/component/notification_button.dart
@@ -1,4 +1,7 @@
 import 'package:movie_ticket_booking_flutter_nlu/core.dart';
+import 'package:movie_ticket_booking_flutter_nlu/handler/http_response.dart';
+import 'package:movie_ticket_booking_flutter_nlu/model/notice.dart';
+import 'package:movie_ticket_booking_flutter_nlu/utilities/StringUtil.dart';
 
 class NotificationButton extends StatefulWidget {
   const NotificationButton({Key? key}) : super(key: key);
@@ -8,8 +11,30 @@ class NotificationButton extends StatefulWidget {
 }
 
 class _NotificationButtonState extends State<NotificationButton> {
+  List<Notice> notices = [];
+  late int numNoticeNotRead = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    final noticeProvider = Provider.of<NoticeProvider>(context, listen: false);
+    final authService = AuthenticationService.instance;
+    authService.getCurrentUserEmail().then((value) => noticeProvider.getNoticeSearch(value).then((value) {
+          HttpResponse response = value;
+          if (response.success) {
+            setState(() {
+              notices = (value.data as List<dynamic>).map((e) => Notice.fromJson(e)).toList();
+              numNoticeNotRead = notices.where((element) => !element.isRead).length;
+            });
+          }
+        }));
+  }
+
   @override
   Widget build(BuildContext context) {
+    final noticeProvider = Provider.of<NoticeProvider>(context, listen: false);
+    final authService = AuthenticationService.instance;
     return Stack(
       children: [
         InkWell(
@@ -18,6 +43,14 @@ class _NotificationButtonState extends State<NotificationButton> {
           splashColor: Colors.transparent,
           highlightColor: Colors.transparent,
           onTap: () {
+            authService.getCurrentUserEmail().then((value) => noticeProvider.readAllNotice(value).then((value) {
+                  HttpResponse response = value;
+                  if (response.success) {
+                    setState(() {
+                      numNoticeNotRead = 0;
+                    });
+                  }
+                }));
             showMenu(
               context: context,
               constraints: const BoxConstraints(minWidth: 400, maxHeight: 500),
@@ -27,20 +60,60 @@ class _NotificationButtonState extends State<NotificationButton> {
                   onTap: () {
                     Navigator.pop(context);
                   },
-                  child: Column(
-                    children: List.generate(
-                      5,
-                      (index) => Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          const Icon(Icons.notifications),
-                          SizedBox(width: getProportionateScreenWidth(10)),
-                          const Text('Thông báo'),
-                        ],
-                      ),
-                    ),
-                  ),
+                  child: notices.isNotEmpty
+                      ? SingleChildScrollView(
+                          child: Column(
+                            children: List.generate(
+                              notices.length,
+                              (index) => Container(
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 5,
+                                  vertical: 10,
+                                ),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    const Icon(Icons.notifications),
+                                    SizedBox(width: getProportionateScreenWidth(10)),
+                                    Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Row(
+                                          children: [
+                                            Text(StringUtil.changeNoticeType(notices[index].type),
+                                                style: const TextStyle(
+                                                  color: Colors.green,
+                                                  fontWeight: FontWeight.bold,
+                                                )),
+                                            const SizedBox(width: 5),
+                                            !notices[index].isRead
+                                                ? const Text('*',
+                                                    style: TextStyle(
+                                                      color: Colors.red,
+                                                      fontSize: 14,
+                                                    ))
+                                                : Container(),
+                                          ],
+                                        ),
+                                        const SizedBox(height: 5),
+                                        Text(
+                                          notices[index].description,
+                                          maxLines: 2,
+                                          softWrap: true,
+                                          textDirection: TextDirection.ltr,
+                                          overflow: TextOverflow.ellipsis,
+                                          textWidthBasis: TextWidthBasis.parent,
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        )
+                      : Container(),
                 ),
               ],
             );
@@ -58,23 +131,34 @@ class _NotificationButtonState extends State<NotificationButton> {
             ),
           ),
         ),
-        Positioned(
+        numNoticeNotRead > 0 ? Positioned(
+                right: 0,
+                top: 0,
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.red,
+                    shape: BoxShape.circle,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(5),
+                    child: Text(
+                      numNoticeNotRead.toString(),
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 10,
+                      ),
+                    ),
+                  ),
+                ),
+              ):  Positioned(
           right: 0,
           top: 0,
           child: Container(
             decoration: const BoxDecoration(
-              color: Colors.red,
               shape: BoxShape.circle,
             ),
-            child: const Padding(
-              padding: EdgeInsets.all(5),
-              child: Text(
-                '1',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 10,
-                ),
-              ),
+            child: Padding(
+              padding: const EdgeInsets.all(5),
             ),
           ),
         ),
